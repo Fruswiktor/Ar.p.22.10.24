add3563f33c73d0d69e8b38d642a045ecc4bfd7a
Fruswiktor <awiktor.scuhanecki@gmail.com>
2024-10-23 10:34:02 +0200

    fafa

--- Pain1.2.ino
+++ Pain1.2.ino
@@ -1,4 +1,5 @@
 #include <M5Tough.h>
+#include <Arduino.h>
 
 ButtonColors on_clrs = {GREEN, WHITE, WHITE};
 ButtonColors off_clrs = {BLACK, WHITE, WHITE};
@@ -7,16 +8,6 @@ Button b2(120, 200, 80, 40, false, "BLUE", off_clrs, on_clrs, MC_DATUM);
 Button b3(220, 200, 80, 40, false, "RESET", off_clrs, on_clrs, MC_DATUM);
 
 uint16_t currentColor = RED;  // Zmienna do przechowywania aktualnego koloru
-
-void eventDisplay(Event& e) {
-  M5.Lcd.fillRect(0, 0, 320, 150, BLACK);
-  Serial.printf("%-12s finger%d  %-18s (%3d, %3d) --> (%3d, %3d)   ",
-                e.typeName(), e.finger, e.objName(), e.from.x, e.from.y,
-                e.to.x, e.to.y);
-
-  Serial.printf("( dir %d deg, dist %d, %d ms )\r\n", e.direction(),
-                e.distance(), e.duration);
-}
 
 void setup() {
   M5.begin();
@@ -50,12 +41,13 @@ void eventDisplayBUTTON3(Event& e) {
 
 void loop() {
   M5.update();
+
   Event& e = M5.Buttons.event;
-  if (e & (E_MOVE | E_RELEASE)) circle(e & E_MOVE ? e.from : e.to, currentColor);
-  if (e & (E_TOUCH | E_MOVE)) circle(e.to, e.finger ? BLUE : currentColor);
+  
+  // Rysowanie kółek natychmiast w odpowiedzi na ruch lub gest
+  if (e & (E_MOVE | E_GESTURE)) circle(e.to, currentColor); 
 }
 
 void circle(Point p, uint16_t c) {
-  M5.Lcd.fillCircle(p.x, p.y, 4, c);
-  M5.Lcd.drawCircle(p.x, p.y, 5, c);
+  M5.Lcd.fillCircle(p.x, p.y, 5, c);
 }
